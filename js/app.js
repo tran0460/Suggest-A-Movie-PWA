e96dff55c0da7746a92e6bbc9a463ceda8f7d64c
tran0460 <tran0460@algonquinlive.com>
2022-02-11 15:21:44 -0500

    created Indexed DB and 2 stores: searchStore and resultStore

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
 const APP = {
+    imageUrl: null,
+    configData: null,
     init: () => {
         APP.registerSW();
+        APP.addListeners();
+        IDB.initDB();
         console.log('init')
+    },
+    getConfig: () => {
+        const url = `https://api.themoviedb.org/3/configuration?api_key=f8950444a4c0c67cbff1553083941ae3`
+        fetch(url) 
+            .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+            throw new Error(`something went wrong : ${response.status}`)
+            }
+        })
+        .then(data => {
+            APP.imageUrl = data.images.secure_base_url
+            APP.configData = data.images
+        })
+        .catch(error => 
+            alert(`Theres been an ERROR!!!!!!!! ${error.name}, ${error.message}`))
+    },
+    addListeners: () => {
+        document.querySelector('.searchBtn').addEventListener('click', SEARCH.handle)
     },
     registerSW: () => {
     navigator.serviceWorker.register('/js/sw.js').catch(function (err) {
@@ -11,5 +35,56 @@ const APP = {
         });
     },
 }
+const SEARCH = {
+    baseUrl : 'https://api.themoviedb.org/3/',
+    api: 'f8950444a4c0c67cbff1553083941ae3',
+    movieList: [],
+    handle: () => {
+        let searchInput = document.querySelector('input').value
+        // const url = `${SEARCH.baseUrl}search/movie?api_key=${SEARCH.api}&query=${searchInput}`
+        let url = `${SEARCH.baseUrl}search/movie?api_key=${SEARCH.api}&query=${searchInput}`;
+        SEARCH.fetch(url)
+    },
+    fetch : (url) => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) throw new Error(`Fetch failed ${response.status}`)
+                return response.json()
+        })
+            .then(data => {
+                console.log(url)
+                console.log(data)
+        })
+            .catch(err => {
+                console.warn(err.message)
+        })
+    },
+}
 
-APP.init()
+const IDB = {
+    DB: null,
+    initDB: () => {
+        let version = 2
+        let dbOpenRequest = indexedDB.open('movieDB', version);
+            dbOpenRequest.onupgradeneeded = function (ev) {
+            DB = ev.target.result; 
+            try {
+                DB.deleteObjectStore('movieStore');
+            } catch (err) {
+                console.log('error deleting old DB');
+            }
+            let searchStore = DB.createObjectStore('searchStore');
+            let resultStore = DB.createObjectStore('resultStore');
+        }
+            dbOpenRequest.onerror = function (err) {
+            console.warn(err.message)
+        }
+            dbOpenRequest.onsuccess = function (ev) {
+
+        }
+    }
+}
+
+const MEDIA = {
+}
+document.addEventListener('DOMContentLoaded', APP.init);
--- js/sw.js
+++ js/sw.js
@@ -37,3 +37,31 @@ self.addEventListener('activate', (ev) => {
         .catch(console.warn)
     );
 });
+self.addEventListener('fetch', (ev) => {
+    ev.respondWith(
+    caches.match(ev.request).then((cacheRes) => {
+        return (
+            cacheRes ||
+            fetch(ev.request)
+            .then((fetchRes) => {
+              //TODO: check here for the 404 error
+                if (! fetchRes.ok) throw new Error(fetchRes.statusText)
+                return caches.open(dynamicCache).then((cache) => {
+                  let copy = fetchRes.clone(); //make a copy of the response
+                  cache.put(ev.request, copy); //put the copy into the cache
+                return fetchRes; //send the original response back up the chain
+                });
+            })
+            .catch((err) => {
+                console.log('SW fetch failed');
+                console.warn(err);
+                if(ev.request.mode === 'navigate') {
+                return caches.match('/404.html').then(cacheRes => {
+                return cacheRes
+                })
+            }
+            })
+        );
+    })
+    );
+});
